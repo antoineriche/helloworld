bdc88efcf3e7bfa10a4e2727c36f7d0b30845cdc
antoine-riche <antoine.riche@rogerhr.ch>
2026-04-14 09:37:42 +0200

    CONF: fix cors config for google login/logout

--- src/main/java/com/ariche/helloworld/config/SecurityConfig.java
+++ src/main/java/com/ariche/helloworld/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.ariche.helloworld.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -29,6 +31,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+            .cors(Customizer.withDefaults())
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(PUBLIC_ENDPOINTS).permitAll()
@@ -37,18 +40,24 @@ public class SecurityConfig {
             .oauth2Login(oauth2 -> oauth2
                 .defaultSuccessUrl(frontEndUrl, true)
             )
+            .logout(logout -> logout
+                .logoutUrl("/api/logout") // URL à appeler depuis le front
+                .logoutSuccessHandler((request, response, authentication) -> {
+                    response.setStatus(HttpStatus.OK.value());
+                })
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+            )
             .build();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(
-            frontEndUrl,
-            "http://localhost:3000"
-        ));
+        configuration.setAllowedOrigins(List.of(frontEndUrl));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
